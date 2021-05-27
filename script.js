82f2a5123e6c292d8c0af79dd3e7eb0d61405afa
Hariket Sukeshkumar Sheth <72455881+hariketsheth@users.noreply.github.com>
2021-05-27 09:32:59 +0530

    Integrated script.js with HTML Form Submit

--- script.js
+++ script.js
@@ -73,3 +73,14 @@ function addReposToCard(repos) {
             reposEl.appendChild(repoEl)
         })
 }
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const user = search.value
+
+    if(user) {
+        getUser(user)
+
+        search.value = ''
+    }
+})
