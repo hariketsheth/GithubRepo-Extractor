2108a7b809e4db97293b466ff67c434f62966740
Hariket Sukeshkumar Sheth <72455881+hariketsheth@users.noreply.github.com>
2021-05-27 09:29:31 +0530

    Added getUser() and getRepos() functions

--- script.js
+++ script.js
@@ -3,3 +3,25 @@ const APIURL = 'https://api.github.com/users/'
 const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
+async function getUser(username) {
+    try {
+        const { data } = await axios(APIURL + username)
+
+        createUserCard(data)
+        getRepos(username)
+    } catch(err) {
+        if(err.response.status == 404) {
+            createErrorCard('No profile with this username')
+        }
+    }
+}
+
+async function getRepos(username) {
+    try {
+        const { data } = await axios(APIURL + username + '/repos?sort=created')
+
+        addReposToCard(data)
+    } catch(err) {
+        createErrorCard('Problem fetching repos')
+    }
+}
